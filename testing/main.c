42c415ced2e30c8872ba2f315e06c2a0e2e85450
mikequ1 <mikequmrsh@gmail.com>
2021-07-09 19:56:49 -0700

    fixed all warnings

--- testing/main.c
+++ testing/main.c
@@ -1,17 +1,19 @@
 #include "shape.h" /* Shape class interface */
 #include <stdio.h> /* for printf() */
 #include <stdlib.h>
+
+
 int main() {
  printf("test\n");
- int* s1 = malloc(shapeSize());
- int* s2 = malloc(shapeSize());
+ Shape* s1 = malloc(shapeSize());
+ Shape* s2 = malloc(shapeSize());
  Shape_ctor(s1, 0, 1);
  Shape_ctor(s2, -1, 2);
  printf("Shape s1(x=%d,y=%d)\n", Shape_getX(s1), Shape_getY(s1));
  printf("Shape s2(x=%d,y=%d)\n", Shape_getX(s2), Shape_getY(s2));
  Shape_moveBy(s1, 2, -4);
  Shape_moveBy(s2, 1, -2);
- printf("Shape s1(x=%d,y=%d)\n", Shape_getX(s1), Shape_getY(&s1));
- printf("Shape s2(x=%d,y=%d)\n", Shape_getX(s2), Shape_getY(&s2));
+ printf("Shape s1(x=%d,y=%d)\n", Shape_getX(s1), Shape_getY(s1));
+ printf("Shape s2(x=%d,y=%d)\n", Shape_getX(s2), Shape_getY(s2));
  return 0;
 }
